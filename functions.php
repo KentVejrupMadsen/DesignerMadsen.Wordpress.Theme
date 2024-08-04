ca5d4c35903550ca304221719ed74f844e09e20d
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2024-08-04 18:37:51 +0200

    removed
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

--- functions.php
+++ functions.php
@@ -1,67 +1,75 @@
 <?php
-/**
- * Twenty Twenty-Two functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_Two
- * @since Twenty Twenty-Two 1.0
- */
-
-
-if ( ! function_exists( 'twentytwentytwo_support' ) ) :
-
 	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
+	 * Twenty Twenty-Two functions and definitions
 	 *
+	 * @link https://developer.wordpress.org/themes/basics/theme-functions/
+	 *
+	 * @package WordPress
+	 * @subpackage Twenty_Twenty_Two
 	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
 	 */
-	function twentytwentytwo_support() {
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-	}
+	if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
-endif;
+		/**
+		 * Sets up theme defaults and registers support for various WordPress features.
+		 *
+		 * @since Twenty Twenty-Two 1.0
+		 *
+		 * @return void
+		 */
+		function twentytwentytwo_support() {
 
-add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+			// Add support for block styles.
+			add_theme_support( 'wp-block-styles' );
 
-if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
+			// Enqueue editor styles.
+			add_editor_style( 'style.css' );
+		}
 
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
+	endif;
 
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-		wp_register_style(
-			'twentytwentytwo-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
+	add_action( 
+		'after_setup_theme', 
+		'twentytwentytwo_support' 
+	);
 
-		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'twentytwentytwo-style' );
-	}
+	if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
-endif;
+		/**
+		 * Enqueue styles.
+		 *
+		 * @since Twenty Twenty-Two 1.0
+		 *
+		 * @return void
+		 */
+		function twentytwentytwo_styles() {
+			// Register theme stylesheet.
+			$theme_version = wp_get_theme()->get( 'Version' );
 
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
+			$version_string = is_string( $theme_version ) ? $theme_version : false;
+			wp_register_style(
+				'twentytwentytwo-style',
+				get_template_directory_uri() . '/style.css',
+				array(),
+				$version_string
+			);
 
-// Add block patterns
-require get_template_directory() . '/inc/block-patterns.php';
+			// Enqueue theme stylesheet.
+			wp_enqueue_style( 
+				'twentytwentytwo-style' 
+			);
+		}
+
+	endif;
+
+	add_action( 
+		'wp_enqueue_scripts', 
+		'twentytwentytwo_styles' 
+	);
+
+	// Add block patterns
+	require get_template_directory() . '/inc/block-patterns.php';
 
 ?>
